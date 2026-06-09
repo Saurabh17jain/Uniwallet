4145e660dd756be5c52c6b4df0a4885fa547d1b6
Saurabh17jain <saurabh1712jain@gmail.com>
2026-06-09 22:06:44 +0530

    Added Newrelic

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,7 +2,6 @@ import {serve} from "inngest/next";
 import {inngest} from "@/lib/inngest/client";
 import {sendDailyNewsSummary, sendSignUpEmail} from "@/lib/inngest/function";
 
-
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [sendSignUpEmail,sendDailyNewsSummary],
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-
+require("newrelic");
 export default function Home() {
   redirect("/sign-in");
 }
